fd8ab13defb8f188dacbff770f707631a3359b14
Ethan Tumbocon <ethan.tumbocon1@gmail.com>
2021-01-22 15:07:03 -0600

    curriculum exercises

--- src/main/java/BeanTest.java
+++ src/main/java/BeanTest.java
@@ -1,6 +1,10 @@
+
 import Models.Albums;
 import Models.Author;
+import Models.Product;
 import Models.Quote;
+
+import java.util.ArrayList;
 
 public class BeanTest {
     public static void main(String[] args) {
@@ -20,11 +24,28 @@ public class BeanTest {
         Author.setFirstName("Douglas");
         Author.setLastName("Adams");
 
-        Quote quote = new Quote();
-        quote.setId(1L);
-        quote.setAuthor(author);
-        quote.setContent("I love deadlines. I love the whooshing noise the y make as they go by.");
-        System.out.println("quote.getAuthor() = " + quote.getAuthor().getFirstName());
+        Quote firstQuote = new Quote();
+        firstQuote.setId(1L);
+        firstQuote.setAuthor(author);
+        firstQuote.setContent("I love deadlines. I love the whooshing noise the y make as they go by.");
+        System.out.println("quote.getAuthor() = " + firstQuote.getAuthor().getFirstName());
 
+        Quote secondQuote = new Quote();
+        secondQuote.setId(2L);
+        secondQuote.setAuthor(author);
+        secondQuote.setContent("Don't panic.");
+
+        ArrayList<Quote> quotes = new ArrayList<>();
+        quotes.add(firstQuote);
+        quotes.add(secondQuote);
+
+        for (Quote q: quotes){
+            System.out.println("quote.getContent() = " + q.getContent());
+            System.out.println("quote.getAuthor().getFirstName() = " + q.getAuthor().getFirstName());
+            System.out.println("quote.getAuthor().getLastName() = " + q.getAuthor().getLastName());
+        }
+
+        DaoFactory.getProductsDao().all();
+        DaoFactory.getProductsDao().insert(new Product());
     }
 }
