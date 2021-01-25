014872343a0529879478640d06d912b327345ee2
Ethan Tumbocon <ethan.tumbocon1@gmail.com>
2021-01-25 15:26:52 -0600

    finished exercises

--- src/main/java/LoginServlet.java
+++ src/main/java/LoginServlet.java
@@ -3,21 +3,37 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebServlet(name = "LoginServlet", urlPatterns = "/login")
 public class LoginServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("/login.jsp").forward(request, response);
+        if(request.getSession().getAttribute("user") != null){
+            response.sendRedirect("/profile");
+            return;
+        }
+        request.getRequestDispatcher("/WEB-INF/login.jsp").forward(request,response);
     }
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
         String username = request.getParameter("username");
         String password = request.getParameter("password");
-        boolean validAttempt = username.equals("admin") && password.equals("password");
 
-        if (validAttempt) {
+        boolean validAttempt = (username.equals("admin") || username.equals("user")) && password.equals("password");
+
+        HttpSession session = request.getSession();
+
+        if (validAttempt && username.equals("admin")) {
+            // Admin path- the person who is logged in is an admin.
+            session.setAttribute("isAdmin", true);
+
+            response.sendRedirect("/admin"); // servlet <-- url pattern
+
+        }else if (validAttempt && username.equals("user")){
+            session.setAttribute("isAdmin",false);
             response.sendRedirect("/profile");
+
         } else {
             response.sendRedirect("/login");
         }
---
+++ src/main/webapp/WEB-INF/logout.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ethantumbocon
+  Date: 1/25/21
+  Time: 1:55 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <jsp:include page="../index.jsp">
+        <jsp:param name="title" value="Logged Out!"/>
+
+    </jsp:include>
+
+</head>
+<body>
+
+</body>
+</html>
